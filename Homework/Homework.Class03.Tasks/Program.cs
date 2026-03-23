d5d8e79a6cf054c8d5529646c9109cb37bda4c7f
David <todorovskidavid23@gmail.com>
2026-03-24 00:16:51 +0100

    Homework 03 and 04 done

---
+++ Homework/Homework.Class03.Tasks/Program.cs
@@ -0,0 +1,58 @@
+﻿#region Task1
+
+int[] integers = new int[6];
+int sum = 0;
+for(int i = 0; i < integers.Length; i++)
+{
+    Console.WriteLine($"Enter integer no.{i+1}");
+    int input = int.Parse(Console.ReadLine());
+
+    if (input % 2 == 0)
+    {
+        sum = sum + input;
+    }
+}
+Console.WriteLine("The result is: "+sum);
+
+
+#endregion
+
+
+#region Task2
+
+string[] studentsG1 =
+{
+    "Zdravko1", "Petko1", "Stanko1", "Branko1", "Trajko1"
+};
+string[] studentsG2 =
+{
+    "Zdravko2", "Petko2", "Stanko2", "Branko2", "Trajko2"
+};
+
+Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+int groupInput = int.Parse(Console.ReadLine());
+
+if (groupInput == 1)
+{
+    Console.WriteLine("The Students in G1 are:");
+    foreach (var student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (groupInput == 2)
+{
+    Console.WriteLine("The Students in G2 are:");
+    foreach (var student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid Input! Please enter 1 or 2.");
+}
+
+
+
+#endregion
---
+++ Homework/Homework.Class04/Program.cs
@@ -0,0 +1,84 @@
+﻿#region Exercise 3
+
+static void Substrings()
+{
+    string msg = "Hello from Avenga Codecademy 2024";
+
+    Console.WriteLine("Enter number of characters:");
+
+    bool isParsed = int.TryParse(Console.ReadLine(), out int numberOfCharacters);
+
+    if (!isParsed)
+    {
+        Console.WriteLine("Invalid input. Please enter a whole number!");
+        return;
+    }
+
+    if (numberOfCharacters <= 0)
+    {
+        Console.WriteLine("You must enter a number greater than 0!");
+        return;
+    }
+
+    if (numberOfCharacters > msg.Length)
+    {
+        Console.WriteLine("You cannot enter more characters than the message contains!");
+        return;
+    }
+
+    string text = msg.Substring(0, numberOfCharacters);
+
+    Console.WriteLine($@"New string: ""{text}""");
+    Console.WriteLine($"Length: {text.Length}");
+}
+
+Substrings();
+
+#endregion
+
+
+#region Exercise 4
+
+DateTime today = DateTime.Now;
+Console.WriteLine(today.AddDays(3));
+Console.WriteLine(today.AddMonths(1));
+Console.WriteLine(today.AddMonths(1).AddDays(3));
+Console.WriteLine(today.AddYears(-1).AddMonths(-2));
+Console.WriteLine(today.ToString("MMMM"));
+Console.WriteLine(today.ToString("yyyy"));
+
+#endregion
+
+
+#region Homework Class 04
+
+static int AgeCalculator(DateTime birthday)
+{
+    DateTime today = DateTime.Now;
+    int age = today.Year - birthday.Year;
+
+    //Дали датумот на раѓање е после датумот што го добиваме кога ќе се вратиме назад од денес за толку години?
+    if (birthday > today.AddYears(-age))
+    {
+        age--;
+    }
+
+    return age;
+}
+
+Console.WriteLine("Enter your birthday (dd.MM.yyyy):");
+
+bool isParsed = DateTime.TryParse(Console.ReadLine(), out DateTime userBirthday);
+
+if (!isParsed)
+{
+    Console.WriteLine("Invalid date input!");
+    return;
+}
+
+int userAge = AgeCalculator(userBirthday);
+
+Console.WriteLine("Your age is: " + userAge);
+
+
+#endregion
